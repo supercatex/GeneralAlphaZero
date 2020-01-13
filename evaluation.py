7e041b92a311af07fe110f752a050e7fce5dd026
Kinda Lam <lamkinun@gmail.com>
2020-01-13 17:49:41 +0800

    evaluation

--- evaluation.py
+++ evaluation.py
@@ -30,68 +30,70 @@ _p2 = GameAgent(2, _model_best, _env)
 _env.add_agent(_p1)
 _env.add_agent(_p2)
 
-# self-play, play with two best model.
-_model_best.load(
-    os.path.join(_dir_model, _filename_config),
-    os.path.join(_dir_model, _filename_weight)
-)
-_model_next.load(
-    os.path.join(_dir_model, _filename_config),
-    os.path.join(_dir_model, _filename_weight)
-)
-_worker = SelfPlayWorker(_env)
-_worker.nb_game_in_file = 100
-_worker.play_data_dir = _dir_data
-_worker.start()
-
-# optimization
-_optimizer = GameOptimizer(_env, _dir_data)
-_optimizer.training()
-
-# evaluation
-while True:
-    _dir_next_model = None
-    for _f1 in os.listdir(_dir_model):
-        _path = os.path.join(_dir_model, _f1)
-        if os.path.isdir(_path):
-            _dir_next_model = _path
-
-    if _dir_next_model is None:
-        break
-
+for k in range(100000000):
+    # self-play, play with two best model.
     _model_best.load(
         os.path.join(_dir_model, _filename_config),
         os.path.join(_dir_model, _filename_weight)
     )
     _model_next.load(
-        os.path.join(_dir_next_model, _filename_config),
-        os.path.join(_dir_next_model, _filename_weight)
+        os.path.join(_dir_model, _filename_config),
+        os.path.join(_dir_model, _filename_weight)
     )
+    _worker = SelfPlayWorker(_env)
+    _worker.nb_game_in_file = 100
+    _worker.play_data_dir = _dir_data
+    _worker.start()
 
-    _total = 20
-    _win = 0
-    _draw = 0
-    for i in range(_total):
-        _env.reset()
-        while not _env.done:
-            agent: Agent = _env.current_agent()
-            action = agent.action(_env)
-            _env.step(action)
-        if _env.winner == 2:
-            _win += 1
-        elif _env.winner == -1:
-            _draw += 1
-        print(f"Game {i + 1}: {_win}/{_total}({_draw})"
-              f"-- {_win / (_total - _draw)} -- {_env.observation.STATES[_env.winner]}")
-        if _win / (_total - _draw) > _threshold:
-            print("Best model changed.")
-            _model_next.save(
-                os.path.join(_dir_model, _filename_config),
-                os.path.join(_dir_model, _filename_weight)
-            )
+    # optimization
+    _optimizer = GameOptimizer(_env, _dir_data)
+    _optimizer.training()
+
+    # evaluation
+    while True:
+        _dir_next_model = None
+        for _f1 in os.listdir(_dir_model):
+            _path = os.path.join(_dir_model, _f1)
+            if os.path.isdir(_path):
+                _dir_next_model = _path
+
+        if _dir_next_model is None:
             break
-        elif (_win + _total - _draw - i) / (_total - _draw) <= _threshold:
-            print("Keep the old one.")
-            break
-    shutil.rmtree(_dir_next_model)
-print("Finished.")
+
+        _model_best.load(
+            os.path.join(_dir_model, _filename_config),
+            os.path.join(_dir_model, _filename_weight)
+        )
+        _model_next.load(
+            os.path.join(_dir_next_model, _filename_config),
+            os.path.join(_dir_next_model, _filename_weight)
+        )
+
+        _total = 50
+        _win = 0
+        _draw = 0
+        for i in range(_total):
+            _env.reset()
+            while not _env.done:
+                agent: Agent = _env.current_agent()
+                action = agent.action(_env)
+                _env.step(action)
+            if _env.winner == 2:
+                _win += 1
+            elif _env.winner == -1:
+                _draw += 1
+            print(f"Game {i + 1}: {_win}/{_total}({_draw})"
+                  f"-- {_win / (_total - _draw) * 100}% -- {_env.observation.STATES[_env.winner]}")
+            if _win / (_total - _draw) > _threshold:
+                print("Best model changed.")
+                _model_next.save(
+                    os.path.join(_dir_model, _filename_config),
+                    os.path.join(_dir_model, _filename_weight)
+                )
+                break
+            elif (_win + _total - _draw - i) / (_total - _draw) <= _threshold:
+                print("Keep the old one.")
+                break
+        shutil.rmtree(_dir_next_model)
+    print(f"Finished {k + 1} rounds.")
+print("END")
